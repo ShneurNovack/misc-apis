2b14b1add601df28398e8f4ffc0709530aa582f7
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2023-06-20 12:26:59 -0400

    Delete build.js

--- build.js
+++
@@ -1,5 +0,0 @@
-// build.js
-const fs = require('fs');
-const workerCode = fs.readFileSync('functions/openai.js', 'utf8');
-const modifiedCode = workerCode.replace('__OPENAI_KEY__', process.env.OPENAI_KEY);
-fs.writeFileSync('worker.js', modifiedCode);
