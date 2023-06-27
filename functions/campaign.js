4b8ba1c596f7c2d9157e38d75e4c9f446296a4a5
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2023-06-27 02:04:00 -0400

    Create campaign.js

---
+++ functions/campaign.js
@@ -0,0 +1,48 @@
+export async function onRequest(context) {
+    const request = context.request || {};
+    const auth_token = 'DXh7xajb97HS3TSA4TIv11m-pwwSveIG';
+
+    if ((request.method || 'GET') !== 'GET') {
+        return new Response(`Method ${request.method} Not Allowed`, { status: 405 });
+    }
+
+    const url = new URL(request.url || '');
+    const campaignID = url.searchParams.get('id') || '';
+
+    if (!campaignID) {
+        return new Response('Missing id', { status: 400 });
+    }
+
+    const apiUrl = `https://api.shneurnovack.com/items/mitzvah_campaigns/${campaignID}?fields=mitzvahs.*,campaign_name,goal`;
+    try {
+        const apiRes = await fetch(apiUrl, {
+            headers: {
+                'Authorization': `Bearer ${auth_token}`
+            }
+        });
+        const apiData = await apiRes.json();
+
+        // Compute the percentage complete and mitzvahs count
+        const totalAmount = apiData.data.mitzvahs.reduce((sum, mitzvah) => sum + mitzvah.amount, 0);
+        const percentComplete = ((totalAmount / Number(apiData.data.goal)) * 100).toFixed(2) + '%';
+        const mitzvahsAmount = apiData.data.mitzvahs.length;
+
+        // Construct the response object
+        const response = {
+            data: {
+                campaign_name: apiData.data.campaign_name,
+                goal: apiData.data.goal,
+                precent_complete: percentComplete,
+                mitzvahs_amount: mitzvahsAmount.toString(),
+            }
+        };
+
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    } catch (error) {
+        return new Response(`Error calling API: ${error.toString()}`, { status: 500 });
+    }
+}
