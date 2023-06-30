d2033db8420dbb5330cc7dc88560daccc961902d
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2023-06-30 11:22:08 -0400

    Create notes.js

---
+++ functions/manage/notes.js
@@ -0,0 +1,43 @@
+export async function onRequest(context) {
+    const request = context.request || {};
+    const auth_token = 'DXh7xajb97HS3TSA4TIv11m-pwwSveIG';
+
+    if ((request.method || 'GET') !== 'GET') {
+        return new Response(`Method ${request.method} Not Allowed`, { status: 405 });
+    }
+
+    const url = new URL(request.url || '');
+    const category = url.searchParams.get('category') || '';
+
+    let apiUrl = `https://api.shneurnovack.com/items/notes?fields=*.*`;
+
+    if (category) {
+        apiUrl += `&filter[campaign][_eq]=${category}`;
+    }
+
+    try {
+        const apiRes = await fetch(apiUrl, {
+            headers: {
+                'Authorization': `Bearer ${auth_token}`
+            }
+        });
+        const apiData = await apiRes.json();
+
+        // Process each data item and format the output
+        const responseData = apiData.data.map(item => ({
+            id: item.id,
+            date_created: item.date_created,
+            title: item.title,
+            body: item.body,
+            category: item.category ? item.category.name : null,
+        }));
+
+        return new Response(JSON.stringify(responseData), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    } catch (error) {
+        return new Response(`Error calling API: ${error.toString()}`, { status: 500 });
+    }
+}
