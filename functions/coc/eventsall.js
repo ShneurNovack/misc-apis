840977382cc05be00c14903950ca34ff4fa9b4f1
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2023-11-28 21:54:49 -0500

    Update eventsall.js

--- functions/coc/eventsall.js
+++ functions/coc/eventsall.js
@@ -1,29 +1,28 @@
 export async function onRequest(context) {
-    const combinedData = [];
+    const fetchPromises = [];
 
     for (let chabadId = 1; chabadId <= 101; chabadId++) {
         const apiUrl = `https://shneurcors.herokuapp.com/https://api.chabadoncampus.org/api/1.0/RsvpEnrollments/${chabadId}/EventSchedules?occurrenceStatus=Upcoming`;
-        
-        try {
-            const apiRes = await fetch(apiUrl);
-
-            if (!apiRes.ok) {
-                console.log(`Request failed for CHABAD-ID ${chabadId}: ${apiRes.status}`);
-                continue; // Skip if the response is not OK
-            }
-
-            const apiData = await apiRes.json();
-            if (apiData && apiData.payload && apiData.payload.results) {
-                combinedData.push(...apiData.payload.results);
-            } else {
-                console.log(`No valid data for CHABAD-ID ${chabadId}`);
-            }
-        } catch (error) {
-            console.log(`Error fetching data for CHABAD-ID ${chabadId}: ${error}`);
-        }
+        fetchPromises.push(
+            fetch(apiUrl)
+            .then(response => response.ok ? response.json() : Promise.reject(`Failed with status ${response.status}`))
+            .catch(error => console.log(`Error fetching data for CHABAD-ID ${chabadId}: ${error}`))
+        );
     }
 
-    return new Response(JSON.stringify(combinedData), {
-        headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+        const allResults = await Promise.all(fetchPromises);
+        const combinedData = allResults.reduce((accumulator, current) => {
+            if (current && current.payload && current.payload.results) {
+                return accumulator.concat(current.payload.results);
+            }
+            return accumulator;
+        }, []);
+
+        return new Response(JSON.stringify(combinedData), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        return new Response(`Error processing requests: ${error.toString()}`, { status: 500 });
+    }
 }
